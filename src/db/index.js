3d4feb410cd6ca0c7a847117162896f918c74de1
Shivangi Maurya <mauryashivangi007@gmail.com>
2026-01-13 15:27:02 +0530

    add util code

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,6 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 
-import express from "express"
-const app = express();
-
 
 const connectDb =  async ()=>{
     try {
--- src/index.js
+++ src/index.js
@@ -1,29 +1,16 @@
-import dotenv from "dotenv"
 import connectDb from "./db/index.js";
-
-dotenv.config({
-    path: './env'
-})
+import { app } from "./app.js";
 
 connectDb()
-
-// import express from "express"
-// const app = express();
-
-
-// ;( async ()=>{
-//     try {
-//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//         app.on("error",(error)=>{
-//             console.log("Error:",error);
-//             throw error
-//         })
-
-//         app.listen(process.env.PORT,()=>{
-//             console.log(`App is listening on port ${process.env.PORT}`);
-//         })
-//     } catch (error) {
-//         console.log(error);
-//         throw error
-//     }
-// })()
+.then(() => {
+    app.on("error", (err) => {
+      console.error("App error:", err);
+      process.exit(1);
+    });
+    app.listen(process.env.PORT || 8000 ,() => {
+        console.log("Server is listening on port",process.env.PORT);
+    })
+})
+.catch((error) => {
+    console.log("Mongoddb connection failed !!",error);
+})
