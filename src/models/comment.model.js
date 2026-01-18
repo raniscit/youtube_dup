a2b1fc7a424cb56fe5f4d473af5e5cfdce72ff52
Shivangi Maurya <mauryashivangi007@gmail.com>
2026-01-18 16:25:39 +0530

    add: create models for comments, likes, playlists, and tweets

---
+++ src/models/comment.model.js
@@ -0,0 +1,21 @@
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema({
+    content:{
+        type:String,
+        required:true
+    },
+    video:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Video"
+    },
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    }
+},{timestamps:true})
+
+commentSchema.plugin(mongooseAggregatePaginate)
+
+export const Comment = mongoose.model("Comment", commentSchema)
